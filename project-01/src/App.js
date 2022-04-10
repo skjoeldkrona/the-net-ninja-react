45ef3d61dbcdcf174e1570c2f3a90eb84326fb1c
skjoeldkrona <skjoeldkrona@gmail.com>
2022-04-10 17:37:11 +0200

    Understading useState

--- project-01/src/App.js
+++ project-01/src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [events, setEvents] = useState([
+    { title: "Mario's birthday bash", id: 1 },
+    { title: "Ewa's live stream", id: 2 },
+    { title: "Race on moo moo farm", id: 3 },
+  ]);
+
+  const handleClick = (id) => {
+    setEvents((prevItem) => {
+      return prevItem.filter((item) => {
+        return id !== item.id;
+      })
+    });
+  };
+
   return (
-  <div>
-    <h1>I am your first react app!</h1>
-  </div>
+    <div className="App">
+      {events.map((item, index) => (
+        <div key={item.id}>
+          <h2>
+            {index + 1}. {item.title}
+          </h2>
+          <button onClick={() => handleClick(item.id)}>Delete item</button>
+        </div>
+      ))}
+    </div>
   );
 }
 
