58de79800b187fd47844d247ece27b8e22f5eba6
skjoeldkrona <skjoeldkrona@gmail.com>
2022-04-23 09:53:44 +0200

    Single recipe page added

--- project-05-recipe-directory/src/pages/recipe/Recipe.js
+++ project-05-recipe-directory/src/pages/recipe/Recipe.js
@@ -1,18 +1,26 @@
 import "./Recipe.css";
 import { useParams } from "react-router-dom";
-import { useFetch } from "../../hooks/useFetch.js"
+import { useFetch } from "../../hooks/useFetch.js";
 
 export default function Recipe() {
   const { id } = useParams();
-  const url = "http://localhost:3000/recipes/" + id
-  const { error, isPending, data: recipe} = useFetch(url)
+  const url = "http://localhost:3000/recipes/" + id;
+  const { error, isPending, data: recipe } = useFetch(url);
 
   return (
     <div className="recipe">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipe && <h1>{recipe.title}</h1>}
-
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook</p>
+          <ul>
+            {recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
-  )
+  );
 }
